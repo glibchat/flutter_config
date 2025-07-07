04abcb4f200a038c9698bcd1eab84a2d9fd4d877
Ashok Madduru <shok@Ashoks-MacBook-Air.local>
2025-07-07 10:10:18 +0530

    fix issue in registrar

--- android/src/main/kotlin/com/byneapp/flutter_config/FlutterConfigPlugin.kt
+++ android/src/main/kotlin/com/byneapp/flutter_config/FlutterConfigPlugin.kt
@@ -1,26 +1,27 @@
 package com.byneapp.flutter_config
 
-import android.app.Activity
 import android.content.Context
 import android.content.res.Resources
 import androidx.annotation.NonNull
 import io.flutter.Log
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 import java.lang.IllegalArgumentException
 import java.lang.reflect.Field
 
-class FlutterConfigPlugin(private val context: Context? = null): FlutterPlugin, MethodCallHandler {
+class FlutterConfigPlugin(private val context: Context? = null) : FlutterPlugin, MethodCallHandler {
 
   private var applicationContext: Context? = context
 
-  private lateinit var channel : MethodChannel
+  private lateinit var channel: MethodChannel
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+  override fun onAttachedToEngine(
+          @NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding
+  ) {
     applicationContext = flutterPluginBinding.applicationContext
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_config")
     channel.setMethodCallHandler(this)
@@ -33,9 +34,9 @@ class FlutterConfigPlugin(private val context: Context? = null): FlutterPlugin, 
 
   companion object {
     @JvmStatic
-    fun registerWith(registrar: Registrar) {
-      val channel = MethodChannel(registrar.messenger(), "flutter_config")
-      channel.setMethodCallHandler(FlutterConfigPlugin(registrar.activity()))
+    fun registerWith(messenger: BinaryMessenger, context: Context) {
+      val channel = MethodChannel(messenger, "flutter_config")
+      channel.setMethodCallHandler(FlutterConfigPlugin(context))
     }
   }
 
@@ -53,12 +54,14 @@ class FlutterConfigPlugin(private val context: Context? = null): FlutterPlugin, 
 
     try {
       val context = applicationContext!!.applicationContext
-      val resId = context.resources.getIdentifier("build_config_package", "string", context.packageName)
-      val className: String = try {
-        context.getString(resId)
-      } catch (e: Resources.NotFoundException) {
-        applicationContext!!.packageName
-      }
+      val resId =
+              context.resources.getIdentifier("build_config_package", "string", context.packageName)
+      val className: String =
+              try {
+                context.getString(resId)
+              } catch (e: Resources.NotFoundException) {
+                applicationContext!!.packageName
+              }
 
       val clazz = Class.forName("$className.BuildConfig")
 
@@ -72,9 +75,7 @@ class FlutterConfigPlugin(private val context: Context? = null): FlutterPlugin, 
         }
       }
 
-      clazz.declaredFields.forEach {
-        variables += it.name to extractValue(it)
-      }
+      clazz.declaredFields.forEach { variables += it.name to extractValue(it) }
     } catch (e: ClassNotFoundException) {
       Log.d("FlutterConfig", "Could not access BuildConfig")
     }
